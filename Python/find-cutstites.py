f9a901071e45b8293a36afdb427009f1faf0014b
Luuk Harbers <l.j.w.harbers@gmail.com>
2020-05-11 14:00:47 +0200

    python script to find cutsites in sequences

---
+++ Python/find-cutstites.py
@@ -0,0 +1,26 @@
+"""
+Get the cutsites in the reference genome (or any other sequence)
+"""
+
+from Bio import SeqIO
+from Bio.Restriction import *
+import re
+
+#specify enzyme
+enzyme = NlaIII
+
+#specify reference
+ref = "/mnt/AchTeraD/Documents/references/hg19/hg19.fa"
+
+#specify output file
+output = "/mnt/AchTeraD/Documents/Projects/scCUTseq/cutsite-distribution/" \
+"hg19-cutsites.tsv"
+
+#loop through chromosomes
+for record in SeqIO.parse(ref, "fasta"):
+	sites = enzyme.search(record.seq)
+	for x in sites:
+		with open(output, "a") as out:
+			out.write(re.sub(" .*", "", record.description) +
+				"\t" + str(x) + "\t" + str(x + 1) + "\n")
+	print(re.sub(" .*", "", record.description) + ": Done")
