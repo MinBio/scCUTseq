825860975735966322f801300959adbe25340664
ljwharbers <l.j.w.harbers@gmail.com>
2020-06-12 11:41:31 +0200

    updated script to find cutsites in a fasta file

--- Python/find-cutsite-in-fasta.py
+++ Python/find-cutsite-in-fasta.py
@@ -7,10 +7,16 @@ query = sys.argv[2].lower()
 
 for record in SeqIO.parse(open(sys.argv[1], 'r'), 'fasta'):
     indexes = []
-    i = str(record.seq).lower().find(query)
-    while i >= 0:
-        indexes.append(str(i + 1))
-        i = str(record.seq).lower().find(query, i + 1)
+    if len(record.seq) > 10000:
+	    i = str(record.seq).lower().find(query)
+	    while i >= 0:
+	        indexes.append(str(i + 1))
+	        i = str(record.seq).lower().find(query, i + 1)
 
-    if len(indexes) > 0:
-        print(record.id + "\t" + ','.join(indexes))
+	    if len(indexes) > 1:
+	    	distances = []
+	    	for i in range(len(indexes)-1):
+	    		dist = int(indexes[i+1]) - int(indexes[i])
+	    		distances.append(dist)
+	    	print(record.id + "\t" + ','.join(str(x) for x in distances))
+	        #print(record.id + "\t" + ','.join(indexes))
