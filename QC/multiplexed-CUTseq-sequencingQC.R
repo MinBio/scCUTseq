30cee83eecc2e8d0d4ee2974b616d69b47db1d28
ljwharbers <l.j.w.harbers@gmail.com>
2020-07-15 09:41:41 +0200

    adjusted multiplexed QC plotting

--- QC/multiplexed-CUTseq-sequencingQC.R
+++ QC/multiplexed-CUTseq-sequencingQC.R
@@ -13,13 +13,13 @@ source("/mnt/AchTeraD/Documents/R-functions/save_and_plot.R")
 # Read in excel files and log file
 
 #top folder
-run_name = "BICRO221"
-top_folder = "/mnt/AchTeraD/data/BICRO221/"
-save_folder = "/mnt/AchTeraD/Documents/Projects/scCUTseq/Plots/sequencing_QC/BICRO221/NEBNext-libs/"
+run_name = "BICRO229"
+top_folder = "/mnt/AchTeraD/data/BICRO229/NEB"
+save_folder = "/mnt/AchTeraD/Documents/Projects/scCUTseq/Plots/sequencing_QC/BICRO229_NEB/"
 
 #list libraries
 libraries = list.dirs(top_folder, recursive = F, full.names = F)
-libraries = libraries[grepl("NZ", libraries)] #can just change the grepl expression in most cases to match library directories
+libraries = libraries[grepl("NZ|MS", libraries)] #can just change the grepl expression in most cases to match library directories
 
 #get input and output reads after barcode/cutsite extraction
 total_reads = data.table(library = character(), input_reads = numeric(), output_reads = numeric())
@@ -44,8 +44,11 @@ for(library in libraries) {
   total_reads = rbind(total_reads, data.table(library = library, input_reads = input_reads, output_reads = output_reads))
   
   #extract per-sample read information for mapping % and duplication
-  pre_dedup = fread(paste0(top_folder, library, "/all.tsv"), select = c(1, 7, 9))
-  post_dedup = fread(paste0(top_folder, library, "/dedup.tsv"), select = 9)
+  pre_file = list.files(paste0(top_folder, library), pattern = "all.tsv", full.names = T)
+  post_file = list.files(paste0(top_folder, library), pattern = "dedup.tsv", full.names = T)
+  
+  pre_dedup = fread(pre_file, select = c(1, 7, 9))
+  post_dedup = fread(post_file, select = 9)
   
   #make DT of reads info
   reads_dt = data.table(library = library, barcode = pre_dedup[[1]], total = pre_dedup[[2]] + pre_dedup[[3]], 
@@ -104,3 +107,4 @@ sheet_text = cbind(run = run_name, yield = paste0(format(sum(total_reads[variabl
                    postdedup = sample_postdedup, dup_perc = format((1 - sample_postdedup / sample_prededup) * 100, digits = 4),
                    over300k = paste0(over300k, "/", samples_perlib), under300k = paste0(under300k, "/", samples_perlib),
                    under50k = paste0(under50k, "/", samples_perlib), success = format(over300k / samples_perlib * 100, digits = 4))
+write.table(sheet_text, paste0(top_folder, "library_overview-sheet.tsv"), quote = F, row.names = F, col.names = T, sep = "\t")
