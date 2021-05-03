fbbb889fafebede6ae7d8ffa55e10f6ddd7e43ed
ljwharbers <l.j.w.harbers@gmail.com>
2021-05-03 13:51:47 +0200

    minor edits

--- Python/find-cutsites.py
+++ Python/find-cutsites.py
@@ -7,14 +7,14 @@ from Bio.Restriction import *
 import re
 
 #specify enzyme
-enzyme = FatI
+enzyme = MseI
 
 #specify reference
-ref = "/mnt/AchTeraD/Documents/references/dengue/NC_001474.2.fasta"
+ref = "/mnt/AchTeraD/Documents/references/hg19/hg19.fa"
 
 #specify output file
-base = "/mnt/AchTeraD/Documents/Projects/COVseq/cutsite-distribution/all-enzymes/"
-output = base + "dengue/dengue_FatI-cutsites.bed"
+base = "/mnt/AchTeraD/Documents/Projects/scCUTseq/cutsite-distribution/"
+output = base + "hg19-MseI-cutsites.bed"
 
 #loop through chromosomes
 for record in SeqIO.parse(ref, "fasta"):
