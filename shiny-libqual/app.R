0def2cdf318da25285d63d59209563bafdc6750f
ljwharbers <l.j.w.harbers@gmail.com>
2021-05-05 16:08:11 +0200

    bugfix in shiny app for annotation

--- shiny-libqual/app.R
+++ shiny-libqual/app.R
@@ -1,80 +1,110 @@
 library(shiny)
 library(data.table)
+library(DT)
+library(shinyFiles)
 
-imgs = list.files("/mnt/AchTeraD/data/BICRO278/MS102/plots/profileplots/500000", pattern=".png", full.names = TRUE)[1:10]
+# List files to rate
+imgs = list.files("/mnt/AchTeraD/data/BICRO278/MS102/plots/profileplots/500000", pattern=".png", full.names = TRUE)
 imgs = imgs[file.exists(imgs) & file.info(imgs)$size > 0]
 
+# Define UI
 ui = fluidPage(
   
-  titlePanel("Slideshow"),
+  # Set title and buttons
+  titlePanel("Cell quality annotation"),
   sidebarLayout(
     sidebarPanel(
       actionButton("good", "Good"),
       actionButton("bad", "Bad"),
       actionButton("intermediate", "Intermediate"),
-      br(),
-      actionButton("table", "Refresh table")
+      actionButton("na", "NA")
     ),
     
+    # Set image and data.table output
     mainPanel(
       imageOutput("image"),
-      br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), 
+      br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
       fluidRow(
         column(1, offset=1, actionButton("previous", "Previous")),
         column(1, offset=1, actionButton("next", "Next")),
         br(),
-        dataTableOutput("table")
+        DT::dataTableOutput("table", width = "75%")
       ),
       
     )
   )
 )
 
+# Server-side calculations
 server = function(input, output, session) {
+
   # Initiate data.table
   dt = data.table(sample = imgs, user_quality = "NA")
-  output$table = renderDataTable(dt)
-  
+  output$table = DT::renderDataTable(dt,
+                                     rownames = FALSE,
+                                     server = FALSE,
+                                     extensions = c("Buttons"),
+                                     options = list(dom = 'Bfrtip', buttons = c('csv')),
+                                     width = "50%")
   # Set parameters
   index = reactiveVal(1)
-  
+
+  # Increase/decrease index after previous/next buttons
   observeEvent(input[["previous"]], {
     index(max(index()-1, 1))
   })
   observeEvent(input[["next"]], {
     index(min(index()+1, length(imgs)))
   })
-  
+
+  # Quality assessment buttons, index increase and output of updated DT
   observeEvent(input[["good"]], {
     i = index()
     dt[i, user_quality := "good"]
     if(index()+1 > length(imgs)) info(dt)
     index(min(index()+1, length(imgs)))
-  })
-  
+    output$table = renderDataTable(dt,
+                                   rownames = FALSE,
+                                   server = FALSE,
+                                   extensions = c("Buttons"),
+                                   options = list(dom = 'Bfrtip', buttons = c('csv')))
+    })
+
   observeEvent(input[["bad"]], {
     i = index()
     dt[i, user_quality := "bad"]
     if(index()+1 > length(imgs)) info(dt)
     index(min(index()+1, length(imgs)))
-  })
-  
+    output$table = renderDataTable(dt,
+                                   rownames = FALSE,
+                                   server = FALSE,
+                                   extensions = c("Buttons"),
+                                   options = list(dom = 'Bfrtip', buttons = c('csv')))  })
+
   observeEvent(input[["intermediate"]], {
     i = index()
     dt[i, user_quality := "intermediate"]
     if(index()+1 > length(imgs)) info(dt)
     index(min(index()+1, length(imgs)))
-  })
-  
-  observeEvent(input[["table"]], {
     output$table = renderDataTable(dt,
-                                   extensions = c("Buttons"), 
-                                   options = list(dom = 'Bfrtip',
-                                                  buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
-                                   ))
-  })
+                                   rownames = FALSE,
+                                   server = FALSE,
+                                   extensions = c("Buttons"),
+                                   options = list(dom = 'Bfrtip', buttons = c('csv')))  })
+  observeEvent(input[["na"]], {
+    i = index()
+    dt[i, user_quality := "NA"]
+    if(index()+1 > length(imgs)) info(dt)
+    index(min(index()+1, length(imgs)))
+    output$table = renderDataTable(dt,
+                                   rownames = FALSE,
+                                   server = FALSE,
+                                   extensions = c("Buttons"),
+                                   options = list(dom = 'Bfrtip', buttons = c('csv')))  })
+
+  # Output of new image after increase/decrease of index
   output$image = renderImage({
-    x = imgs[index()] 
+    x = imgs[index()]
     list(src = x, alt = "alternate text", width = "75%", heigth = "75%")
   }, deleteFile = FALSE)
 }
